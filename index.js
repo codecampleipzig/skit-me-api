1e5cf7f80a3f3cec75a90ae2147c22d015c9f3b0
Moritz Piehl <moritz.piehl@bluewin.ch>
2020-06-06 18:59:31 +0200

    Add random room id, room update, disconnect boilerplate as good as we could

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const koa = require("koa");
 const koaRouter = require("koa-router");
 const socketIo = require("socket.io");
 const cors = require("@koa/cors");
+const { v4: uuidv4 } = require("uuid");
 
 const app = new koa();
 const router = new koaRouter();
 
-router.get("koala", "/", ctx => {
-  ctx.body = "Welcome! To the Koala Book of Everything!";
-});
+const rooms = {};
 
-router.post("/rooms", ctx => {
+router.post("/rooms", (ctx) => {
+  // generate random unique room id with uuidv4
+  const roomId = uuidv4();
+  // create empty room for roomId
+  rooms[roomId] = { roomId, players: [] };
+  // send generated room id to player so that he/she can reenter the room
   ctx.body = {
-    roomId: 3
+    roomId,
   };
 });
 
@@ -23,21 +27,44 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const server = app.listen(1234, () => console.log("running on port 1234"));
 const io = socketIo(server);
-const room = {
-  players: [],
-  roomId: 3
-};
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on("joinRoom", (data, respond) => {
-    console.log(data);
-    room.players.push(data.userName);
-    respond(room);
-    socket.broadcast.emit("roomUpdate", room);
+  socket.on("joinRoom", ({ userName, roomId }, respond) => {
+    const room = rooms[roomId];
+    if (!room) {
+      respond({
+        error: "room does not exist",
+      });
+
+      return;
+    }
+
+    socket.join(roomId, () => {
+      const userId = uuidv4();
+      room.players.push({
+        userName,
+        socketId: socket.id,
+        connected: true,
+        ready: false,
+      });
+      respond(room);
+      socket.to(roomId).emit("roomUpdate", room);
+    });
   });
 
   socket.on("disconnect", () => {
     console.log("a user disconnected");
+    console.log(socket.rooms);
+    const socketRooms = Object.keys(socket.rooms);
+    if (socketRooms.length) {
+      // if player disconnects kick him out of the room
+      const roomId = socketRooms[0];
+      console.log(roomId);
+      const room = rooms[roomId];
+      const player = room.players.find(player.socketId == socket.id);
+      player.connected = false;
+      socket.to(roomId).emit("roomUpdate", room);
+    }
   });
 });
