b90cb9ed1fa1b98cb37a6db54cf7121261f03351
Moritz Piehl <moritz.piehl@bluewin.ch>
2020-06-06 13:38:39 +0200

    add socket.io

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 const koa = require('koa')
 const koaRouter = require('koa-router')
+const socketIo = require('socket.io')
+
 
 const app = new koa()
 const router = new koaRouter()
+
+
 
 router.get('koala', '/', (ctx) => {
   ctx.body = "Welcome! To the Koala Book of Everything!"
@@ -12,4 +16,16 @@ router.get('koala', '/', (ctx) => {
 app.use(router.routes())
   .use(router.allowedMethods())
 
-app.listen(1234, () => console.log('running on port 1234'));
+const server = app.listen(1234, () => console.log('running on port 1234'));
+const io = socketIo(server)
+io.on('connection', (socket) => {
+  socket.on("hello", (data)=>{
+    console.log(data);
+    socket.emit("hello back","hello from server")
+    console.log("sent hello from server")
+  })
+  socket.on('disconnect',()=>{
+    console.log("a user disconnected")
+  })
+  console.log('a user connected');
+});
