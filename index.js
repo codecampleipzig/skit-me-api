9449c4b68647eb2a88db143ee9555a3d4903a43d
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-06-06 23:12:31 +0200

    Prototype implementation

--- index.js
+++ index.js
@@ -8,19 +8,59 @@ const app = new koa();
 const router = new koaRouter();
 
 const rooms = {};
+const NUM_ROUNDS = 3;
 
-const users = {};
+function getResultsFromHistory(history) {
+  console.log(history);
+  return history[0].results.map(seed => {
+    let { user } = seed;
+    console.log(user);
+    const sheet = [];
 
+    history.forEach(phase => {
+      console.log("user", user);
+      const resultForSheet = phase.results.find(
+        result => result.user.socketId == user.socketId
+      );
+      sheet.push(
+        phase.name == "DrawingPhase"
+          ? {
+              type: "drawing",
+              drawingURL: resultForSheet.drawingURL
+            }
+          : {
+              type: "writing",
+              descriptionTitle: resultForSheet.descriptionTitle
+            }
+      );
+      console.log("sheet", sheet);
+      user = resultForSheet.nextPlayer;
+    });
+    return sheet;
+  });
+}
 router.post("/rooms", ctx => {
   // generate random unique room id with uuidv4
   const roomId = uuidv4();
   // create empty room for roomId
-  rooms[roomId] = { roomId, players: [] };
+  rooms[roomId] = { roomId, players: [], game: null };
   // send generated room id to player so that he/she can reenter the room
   ctx.body = {
     roomId
   };
 });
+
+function sanitizeRoom(room) {
+  const { players, roomId } = room;
+  return {
+    roomId,
+    players: players.map(({ userName, ready, connected }) => ({
+      userName,
+      ready,
+      connected
+    }))
+  };
+}
 
 // install router to app
 app.use(cors());
@@ -33,6 +73,7 @@ const io = socketIo(server);
 io.on("connection", socket => {
   console.log("a user connected");
   let room = null;
+  let user = null;
   socket.on("joinRoom", ({ userName, roomId }, respond) => {
     if (room) {
       respond({
@@ -49,26 +90,107 @@ io.on("connection", socket => {
     }
 
     socket.join(roomId, () => {
-      room.players.push({
+      user = {
         userName,
         socketId: socket.id,
+        socket,
         connected: true,
         ready: false
-      });
+      };
+      room.players.push(user);
 
       respond({
-        room
+        room: sanitizeRoom(room)
       });
-      socket.to(roomId).emit("roomUpdate", room);
+      socket.to(roomId).emit("roomUpdate", sanitizeRoom(room));
     });
+  });
+
+  socket.on("ready", () => {
+    if (!room) return;
+
+    user.ready = true;
+
+    io.to(room.roomId).emit("roomUpdate", sanitizeRoom(room));
+    if (room.players.every(player => player.ready)) {
+      room.game = {
+        players: room.players.filter(player => player.connected),
+        stage: {
+          name: "GameSeedPhase",
+          results: []
+        },
+        history: []
+      };
+      io.to(room.roomId).emit("startGame");
+    }
+  });
+
+  socket.on("completeWriting", descriptionTitle => {
+    room.game.stage.results.push({
+      user,
+      descriptionTitle
+    });
+    if (room.game.stage.results.length == room.game.players.length) {
+      room.game.history.push(room.game.stage);
+
+      if (room.game.history.length == NUM_ROUNDS) {
+        const results = getResultsFromHistory(room.game.history);
+        io.to(room.roomId).emit("endGame", results);
+      } else {
+        for (let i = 0; i < room.game.players.length; i += 1) {
+          const player = room.game.players[i];
+          const sheet = room.game.stage.results.find(
+            result => result.user.socketId == player.socketId
+          );
+          const nextIndex = i == room.game.players.length - 1 ? 0 : i + 1;
+          const nextPlayer = room.game.players[nextIndex];
+          sheet.nextPlayer = nextPlayer;
+          nextPlayer.socket.emit("startDrawing", sheet.descriptionTitle);
+        }
+
+        room.game.stage = {
+          name: "DrawingPhase",
+          results: []
+        };
+      }
+    }
+  });
+  socket.on("completeDrawing", drawingURL => {
+    room.game.stage.results.push({
+      user,
+      drawingURL
+    });
+    if (room.game.stage.results.length == room.game.players.length) {
+      room.game.history.push(room.game.stage);
+
+      if (room.game.history.length == NUM_ROUNDS) {
+        const results = getResultsFromHistory(room.game.history);
+        io.to(room.roomId).emit("endGame", results);
+      } else {
+        for (let i = 0; i < room.game.players.length; i += 1) {
+          const player = room.game.players[i];
+          const sheet = room.game.stage.results.find(
+            result => result.user.socketId == player.socketId
+          );
+          const nextIndex = i == room.game.players.length - 1 ? 0 : i + 1;
+          const nextPlayer = room.game.players[nextIndex];
+          sheet.nextPlayer = nextPlayer;
+          nextPlayer.socket.emit("startWriting", sheet.drawingURL);
+        }
+
+        room.game.stage = {
+          name: "WritingPhase",
+          results: []
+        };
+      }
+    }
   });
 
   socket.on("disconnect", () => {
     if (room) {
-      const user = room.players.find(player => player.socketId == socket.id);
       user.connected = false;
       user.socketId = null;
-      socket.to(room.roomId).emit("roomUpdate", room);
+      socket.to(room.roomId).emit("roomUpdate", sanitizeRoom(room));
     }
   });
 });
