fc461ed04787d8ee592e00b7b3ba72e92641994f
Moritz Piehl <moritz.piehl@bluewin.ch>
2020-06-07 12:01:33 +0200

    Add completeDrawing

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const router = new KoaRouter();
 
 const rooms = {};
 
-router.post("/rooms", ctx => {
+router.post("/rooms", (ctx) => {
   // generate random unique room id with uuidv4
   const roomId = uuidv4();
   // create empty room for roomId
   rooms[roomId] = { roomId, players: [], game: null };
   // send generated room id to player so that he/she can reenter the room
   ctx.body = {
-    roomId
+    roomId,
   };
 });
 
@@ -30,20 +30,20 @@ app.use(router.routes()).use(router.allowedMethods());
 const server = app.listen(PORT, () => console.log(`running on port ${PORT}`));
 const io = socketIo(server);
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log("a user connected");
   let room = null;
   let user = 0;
   socket.on("joinRoom", ({ userName, roomId }, respond) => {
     if (room) {
       respond({
-        error: "already connected"
+        error: "already connected",
       });
     }
     room = rooms[roomId];
     if (!room) {
       respond({
-        error: "room does not exist"
+        error: "room does not exist",
       });
 
       return;
@@ -54,12 +54,12 @@ io.on("connection", socket => {
         userName,
         socketId: socket.id,
         connected: true,
-        ready: false
+        ready: false,
       };
       room.players.push(user);
 
       respond({
-        room
+        room,
       });
       socket.to(roomId).emit("roomUpdate", room);
     });
@@ -69,27 +69,39 @@ io.on("connection", socket => {
     user.ready = true;
     io.to(room.roomId).emit("roomUpdate", room);
 
-    if (room.players.every(player => player.ready)) {
+    if (room.players.every((player) => player.ready)) {
       room.game = {
         stage: {
           name: "GameSeedPhase",
-          results: []
+          results: [],
         },
-        history: []
+        history: [],
       };
       io.to(room.roomId).emit("startSeed");
     }
   });
-  socket.on("completeWriting", descriptionTitle => {
+  socket.on("completeWriting", (descriptionTitle) => {
     room.game.stage.results.push({
       user,
-      descriptionTitle
+      descriptionTitle,
     });
 
     if (room.game.stage.results.length == room.players.length) {
       room.game.history.push(room.game.stage);
       io.to(room.roomId).emit("startDrawing", descriptionTitle);
       room.game.stage = { name: "DrawingPhase", results: [] };
+    }
+  });
+  socket.on("completeDrawing", (drawingURL) => {
+    room.game.stage.results.push({
+      user,
+      drawingURL,
+    });
+
+    if (room.game.stage.results.length == room.players.length) {
+      room.game.history.push(room.game.stage);
+      io.to(room.roomId).emit("startWriting", drawingURL);
+      room.game.stage = { name: "WritingPhase", results: [] };
     }
   });
 
